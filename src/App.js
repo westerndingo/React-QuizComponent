aed4e58db40bfc0ac8b9bc2eb65f4a6086182949
westerndingo <westerndingo@gmail.com>
2018-09-02 22:14:27 -0700

    Completed module 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import React, { Component } from 'react'
-import './App.css'
+import React, { Component } from 'react';
+import './App.css';
+import Quiz from './Quiz';
 
 class App extends Component {
   render() {
-    return (
-      <div />
-    )
+    return <Quiz />;
   }
 }
 
-export default App
+export default App;
